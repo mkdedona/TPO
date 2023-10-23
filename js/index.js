a6391e3434d4d01dfd1405cd74222cb3c7530e31
Mauro Karim De Doná <128740829+mkdedona@users.noreply.github.com>
2023-10-23 18:40:39 +0000

    agregado menu y json con js

--- js/index.js
+++ js/index.js
@@ -24,21 +24,33 @@ diaReserva.value = hoy.toISOString().split('T')[0];
 diaReserva.setAttribute('min', diaReserva.value);
 
 let submitBtn = document.querySelector(".submitBtn");
-formulario.addEventListener("submit", fnClick);
+submitBtn.addEventListener("click", fnClick);
 
 let itemForm = document.querySelectorAll(".itemForm");
 
 
 function fnClick(e) {
     e.preventDefault();
-    let isValid = true;
-    if( (!itemForm[0].value)||(!itemForm[1].value)||(!itemForm[2].value)||(!itemForm[3].value)||(!itemForm[4].value)||(!itemForm[5].value) ){
-        isValid = false;
-        alert('Por favor, complete todos los datos.');
+
+    if( (itemForm[0].value=="")||(itemForm[1].value=="")||(itemForm[2].value=="")||(itemForm[3].value=="")||(itemForm[4].value=="")||(itemForm[5].value=="") ){
+        Swal.fire({
+            icon: 'error',
+            title: 'Oops...',
+            text: 'Por favor, complete todos los datos.'
+          })
     }else{
-        alert("Tu reserva se confirmó exitosamente. ¡Nos vemos pronto!");
+        Swal.fire({
+            icon: 'success',
+            title: 'Reserva Confirmada',
+            title: 'Tu reserva se confirmó exitosamente. ¡Nos vemos pronto!'
+            });
+        itemForm.forEach((input) => {
+            input.value = "";
+            itemForm[7].value="Enviado";            
+        });
     }
-}
+    
+    }
 
 
 
